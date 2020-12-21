fe778e4f50158e2954d05f106266a5eb376812fe
George <me@gdonnelly.us>
2020-12-20 19:23:41 -0500

    init

---
+++ Infrastructure/DBMigration/20201220_CreateLogTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace Infrastructure.DBMigration
+{
+    [Migration(20201220)]
+    public class CreateLogTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Log")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Text").AsString();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Log");
+        }
+    }
+}
